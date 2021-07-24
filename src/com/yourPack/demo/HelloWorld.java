7b992b07d653cd1ecab9d65e38361116c5fa35a8
AaronQi <aaronqk@mails.qust.edu.cn>
2021-07-24 22:00:31 +0800

    Initial commit

---
+++ src/com/yourPack/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.yourPack.demo;
+
+
+public class HelloWorld {
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
